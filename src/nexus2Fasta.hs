2d075829a67b3c7a35862a89e2e75095f6e5a6c2
Ward Wheeler <ward@system76-pc.localdomain>
2025-02-20 15:41:53 -0500

    Edits to nexus2Fasta

--- src/nexus2Fasta.hs
+++ src/nexus2Fasta.hs
@@ -1,15 +1,54 @@
---File to read nexus file and return fasta file
+{- |
+Module      :  nexus2Fasta
+Description :  Converts nexus to fasta file
+Copyright   :  (c) 2025 Ward C. Wheeler, Division of Invertebrate Zoology, AMNH. All rights reserved.
+License     :  
 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+
+Maintainer  :  Ward Wheeler <wheeler@amnh.org>
+Stability   :  unstable
+Portability :  portable (I hope)
+
+-}
+
+module Main where
+
+import System.IO
+import System.Process
+import System.Environment
 import Data.List
 import Debug.Trace
 
---printFasta takes list of two strings (name and sequence) and 
---prints with '>' etc
-printFasta :: [[String]] -> IO ()
+-- | printFasta takes list of two strings (name and sequence) and 
+-- prints with '>' etc
+printFasta :: [String] -> IO ()
 printFasta stuff =
     if null stuff then putStr ""
     else 
-        let curStuff = head stuff
+        let curStuff = words $ head stuff
         in
         if length curStuff < 2 then 
             do
@@ -19,7 +58,7 @@ printFasta stuff =
                 putStr (">" ++ (head curStuff) ++ "\n" ++ (last curStuff) ++ "\n")
                 printFasta (tail stuff)
 
---subRN subsitutes /n for /r so lines will work
+-- | subRN subsitutes /n for /r so lines will work
 subRN :: String -> String
 subRN inString =
     if null inString then []
@@ -29,17 +68,25 @@ subRN inString =
         if (inChar == '\r') then '\n' : (subRN (tail inString))
         else inChar : (subRN $ tail inString)
 
+-- | getGuts removes initial stuff through "matrix" line and end stuff after ";"
+getGuts :: String -> [String]
+getGuts inString =
+    let inWords = lines inString
+        notStartList = dropWhile (/= "matrix") inWords
+        notStartList' = drop 1 notStartList
+        middlePart = takeWhile (/= ";") notStartList'
+    in middlePart
 
---read fasta file
+
+-- | Main function for conversion
 main :: IO ()
 main = 
   do 
      allFile <-  getContents
-     --print $ length $ lines allFile 
-     let allFileProcessed = subRN allFile
-     let rawGuts = filter (/= '\t') $ filter (/= '\'') allFileProcessed
-     let guts = lines $ takeWhile (/= ';') rawGuts -- $ unlines $ drop 8 $ lines rawGuts
-     --print guts
-     let wordGuts = map words guts
-     --print wordGuts
-     printFasta wordGuts
+     hPutStrLn stderr $ "There are " <> (show $ length $ lines allFile) <> " lines in the input nexus file" 
+     -- let allFileProcessed = subRN allFile
+     -- let rawGuts = filter (/= '\t') $ filter (/= '\'') allFileProcessed
+     let guts = getGuts allFile -- lines $ takeWhile (/= ';') rawGuts -- $ unlines $ drop 8 $ lines rawGuts
+     --mapM_ (hPutStrLn stderr) guts
+     hPutStrLn stderr $ "There are " <> (show $ length guts) <> " sequences to output"
+     printFasta guts
